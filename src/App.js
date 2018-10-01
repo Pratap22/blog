2cc6a39adfb1245a77da9f980399b4bf2d0b83f7
pratap22 <sharma.pratap22@gmail.com>
2018-10-01 11:43:36 +0530

    Few updates

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import BlogHeader from './Components/BlogHeader/BlogHeader.js'
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <BlogHeader />
+                <About />
+                {/* <Signin />
+                <Register /> */}
+            </div>
+        )
+
+    }
+}
---
+++ src/Components/BlogHeader/BlogHeader.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BlogHeader = () => {
+    return (
+        <div>
+            <h1>Its Happening</h1>
+        </div>
+    )
+}
