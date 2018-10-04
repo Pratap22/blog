d7a95c49f5888eec58a5f365b0f9e6e832f25ddc
pratap22 <sharma.pratap22@gmail.com>
2018-10-04 14:19:14 +0530

    Few updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import BlogHeader from './Components/BlogHeader/BlogHeader.js';
 import About from './Components/About/About.js';
 import Dropdown from './Components/Dropdown/Dropdown.js';
 
 class App extends Component {
-    render(){
-        return (
-            <div>
-                <BlogHeader />
-                <Dropdown />
-                <About />
-                {/* <Signin />
+  render() {
+    return (
+      <div>
+        <BlogHeader />
+        <Dropdown />
+        <About />
+        {/* <Signin />
                 <Register /> */}
-                <footer className='measure center'> Website Designed by Pratap Sharma</footer>
-            </div>
-        )
-
-    }
+        <footer className="measure center bottom"> Website Designed by Pratap Sharma</footer>
+      </div>
+    );
+  }
 }
-export default App
+export default App;
